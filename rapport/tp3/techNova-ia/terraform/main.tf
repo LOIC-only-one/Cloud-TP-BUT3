0f164905e1ca9616675e6df7d5367ad336fcdde4
MAURER <loicmaurer0@gmail.com>
2026-01-13 08:40:48 +0100

    gitignore

---
+++ rapport/tp3/techNova-ia/terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.0"
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-3"  # Paris
+  
+  default_tags {
+    tags = {
+      Project     = "TechNova-Phase2"
+      Environment = "dev"
+      ManagedBy   = "Terraform"
+    }
+  }
+}
